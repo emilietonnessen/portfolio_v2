f87862c3ff8a426a78624fff53d86242fdf0133b
Emizzari <emilie@tonnessen.com>
2021-03-20 15:47:48 +0100

    Adding landingpage to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Layout/Footer"
 import Sidebar from "./components/Layout/Sidebar";
+import Landingpage from "./containers/Landingpage";
 
 const App = () => {
     return (
         <div className="app">
             <Sidebar />
+            <Landingpage />
             
             <Footer />
         </div>
