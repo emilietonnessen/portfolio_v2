7790332460666352371ee61d9b21d83d4cc60a56
Emizzari <emilie@tonnessen.com>
2021-03-20 15:11:28 +0100

    Creating a Footer Component for the layout

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer className="footer">
+            <p className="footer__text">{currentYear} &copy; Emilie Tonnessen. All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer
