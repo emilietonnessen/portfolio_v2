81032862d7341f0b7bf62380370bfca673cad06c
Emizzari <emilie@tonnessen.com>
2021-03-20 15:47:40 +0100

    Fixing Class bug

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -12,23 +12,23 @@ const Sidebar = () => {
             <nav className="nav">
                 <ul className="nav__list">
                     <li className="nav__item nav__item--active">
-                        <a href="#portfolio" className="nav__link"><i class="fas fa-palette"></i> Portfolio</a>
+                        <a href="#portfolio" className="nav__link"><i className="fas fa-palette"></i> Portfolio</a>
                     </li>
                     <li className="nav__item">
-                        <a href="#about" className="nav__link"><i class="fas fa-user"></i> about</a>
+                        <a href="#about" className="nav__link"><i className="fas fa-user"></i> about</a>
                     </li>
                     <li className="nav__item">
-                        <a href="#contact" className="nav__link"><i class="fas fa-envelope"></i> contact</a>
+                        <a href="#contact" className="nav__link"><i className="fas fa-envelope"></i> contact</a>
                     </li>
                 </ul>
             </nav>
 
             <div className="media-links">
                 <a href="https://github.com/Emizzari" className="media-links__link" target="_blank" rel="noreferrer">
-                    <i class="fab fa-github"></i>
+                    <i className="fab fa-github"></i>
                 </a>
                 <a href="https://www.linkedin.com/in/emilie-henriette-t%C3%B8nnessen-19a850191/" className="media-links__link" target="_blank" rel="noreferrer">
-                    <i class="fab fa-linkedin"></i>
+                    <i className="fab fa-linkedin"></i>
                 </a>
             </div>
         </div>
