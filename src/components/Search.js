cd25e63fc71beb2ff18892bf4d1a00f55613e553
Emizzari <emilie@tonnessen.com>
2021-03-21 12:05:19 +0100

    Adding search

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,9 @@ const Search = () => {
     return (
         <div className="search">
             <input type="text" placeholder="Search.." className="search__input" />
-            <button className="search__icon"></button>
+            <button className="search__button">
+                <i className="search__icon far fa-search"></i>
+            </button>
         </div>
     )
 }
