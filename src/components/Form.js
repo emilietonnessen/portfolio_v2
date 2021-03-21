2a553cead9f08f3e09d3d5ac616f3aab32d0f02a
Emizzari <emilie@tonnessen.com>
2021-03-21 16:37:54 +0100

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,20 @@
+import Button from "./UI/Button";
 
 
 const Form = () => {
     return (
-        <form>
-            This is my form!
+        <form className="contact-form">
+            <input className="contact-form__input" type="text" placeholder="Kari Nordman"/>
+
+            <input className="contact-form__input" type="text" placeholder="kari@nordman.no"/>
+
+            <input className="contact-form__input" type="text" placeholder="Topic"/>
+
+            <textarea className="contact-form__textarea" name="message" id="message" placeholder="Your Message.."></textarea>
+
+            <Button type="submit" link="#contact">send</Button>
         </form>
-    )
+    );
 }
 
 export default Form;
