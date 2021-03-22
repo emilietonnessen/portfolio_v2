8e4f13fabbe75ee156851d88ccb528962c49487c
Emizzari <emilie@tonnessen.com>
2021-03-22 16:28:55 +0100

    Update Portfolio.js

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -1,5 +1,13 @@
-import React from 'react'
+import Project from '../components/project/Project'
+import ProjectTall from '../components/project/ProjectTall'
+import ProjectWide from '../components/project/ProjectWide'
 import Search from '../components/Search'
+import risorklatreklubb from '../assets/risorklatreklubb.png';
+import varus from '../assets/varus.jpg';
+import natours from '../assets/natours.png';
+import csm from '../assets/csm.png';
+import recycle from '../assets/recycle.jpg';
+import spacex from '../assets/spacex.jpg';
 
 const Portfolio = () => {
     return (
@@ -12,24 +20,39 @@ const Portfolio = () => {
             <Search />
 
             <div className="projects">
-                <div className="project project--1">
-                    
-                </div>
-                <div className="project project--2">
-                    
-                </div>
-                <div className="project project--3">
-                    
-                </div>
-                <div className="project project--4">
-                   
-                </div>
-                <div className="project project--5">
-                   
-                </div>
-                <div className="project project--6">
-                   
-                </div>
+                
+                <Project 
+                    image={risorklatreklubb}
+                    title="Risør Klatreklubb"
+                    id="1" />
+                 
+                
+                <Project 
+                    image={varus}
+                    title="Varus Cosmetics"
+                    id="2" />
+                
+                <ProjectTall
+                    image={spacex}
+                    title="Space X"
+                    id="3" />
+                
+                <ProjectTall
+                    image={csm}
+                    title="CMS"
+                    id="4" />
+                
+
+                <Project 
+                    image={natours}
+                    title="Natours"
+                    id="5" />
+
+                <ProjectWide
+                    image={recycle}
+                    title="Re-Cycle"
+                    id="6" />
+                
             </div>
 
         </div>
