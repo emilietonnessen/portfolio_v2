5d6af57ba9b4e753c563461527ad9ac5bb548f69
Emizzari <emilie@tonnessen.com>
2021-03-22 16:28:47 +0100

    Create Project.js

---
+++ src/components/project/Project.js
@@ -0,0 +1,23 @@
+import Button from "../UI/Button"
+
+
+const Project = ({image, title, id}) => {
+    return (
+        <div className={`project project--${id}`}>
+            <img src={image} alt="test" className="project__img"/>
+
+            <div className="project__content">
+                <h5 className="project__title">
+                    {title}
+                </h5>
+                <div className="project__buttons">
+                    <Button link="https://github.com/Emizzari/risor-klatreklubb">Github</Button>
+                    <Button link="https://www.tonnessen.com/projects/risor-klatreklubb/">Live Demo</Button>
+                </div>
+                
+            </div>
+        </div>
+    );
+}
+
+export default Project;
