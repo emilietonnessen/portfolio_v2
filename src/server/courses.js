66d91589043a64fb247dc23571e6d132dc76c8db
Emizzari <emilie@tonnessen.com>
2021-03-25 11:34:11 +0100

    Creating an array with courses

---
+++ src/server/courses.js
@@ -0,0 +1,52 @@
+const courses = [
+    {
+        'id': 1,
+        'slug': 'advanced-css',
+        'name': 'Advanced CSS and Sass',
+        'link': 'https://www.udemy.com/course/advanced-css-and-sass/',
+        'certificate': 'https://www.tonnessen.com/assets/certificate_advanced-css-and-sass.jpg',
+        'progress': null
+    },
+    {
+        'id': 2,
+        'slug': 'react-the-complete-guide',
+        'name': 'React - The Complete Guide',
+        'link': 'https://www.udemy.com/course/react-the-complete-guide-incl-redux/',
+        'certificate': null,
+        'progress': '50%'
+    },
+    {
+        'id': 3,
+        'slug': 'react-and-typescript',
+        'name': 'React and TypeScript',
+        'link': 'https://www.udemy.com/course/react-and-typescript-build-a-portfolio-project/',
+        'certificate': null,
+        'progress': '10%'
+    },
+    {
+        'id': 4,
+        'slug': 'advanced-javascript',
+        'name': 'The Complete JavaScript course 2021',
+        'link': 'https://www.udemy.com/course/the-complete-javascript-course/',
+        'certificate': null,
+        'progress': '0%'
+    },
+    {
+        'id': 5,
+        'slug': 'node-express-mongodb',
+        'name': 'Node.js, Express, MongoDB & More',
+        'link': 'https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/',
+        'certificate': null,
+        'progress': '0%'
+    },
+    {
+        'id': 6,
+        'slug': 'wordpress',
+        'name': 'Become a WordPress Developer',
+        'link': 'https://www.udemy.com/course/become-a-wordpress-developer-php-javascript/',
+        'certificate': null,
+        'progress': '20%'
+    }
+]
+
+export default courses;
