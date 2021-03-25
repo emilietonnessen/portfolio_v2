7cbe05cea663673cdc0f61171bb49d711d20896d
Emizzari <emilie@tonnessen.com>
2021-03-25 11:34:18 +0100

    created an array with projects

---
+++ src/server/projects.js
@@ -0,0 +1,87 @@
+const projects = [
+    {
+        'id': 1,
+        'slug': 'risor-klatreklubb',
+        'name': 'Risør Klatreklubb',
+        'description': 'lorem ipsum',
+        'thumbnail': 'https://www.tonnessen.com/assets/risorklatreklubb.png',
+        'link': 'https://www.tonnessen.com/projects/risor-klatreklubb/',
+        'github': 'https://github.com/Emizzari/risor-klatreklubb',
+        'school': null,
+        'languages': ['HTML', 'Sass', 'JavaScript', 'PHP']
+    },
+    {
+        'id': 2,
+        'slug': 'varus-cosmetics',
+        'name': 'Varus Cosmetics',
+        'description': 'lorem ipsum',
+        'thumbnail': 'https://www.tonnessen.com/assets/varus.jpg',
+        'link': 'https://www.tonnessen.com/noroff/feu/semester-project-2/',
+        'github': 'https://github.com/Emizzari/emilie-tonnessen_semester-project-2',
+        'school': {
+            'grade': 'A',
+            'brief': 'This was a School assignment.'
+        },
+        'languages': ['HTML', 'Sass', 'JavaScript']
+    },
+    {
+        'id': 3,
+        'slug': 'natours',
+        'name': 'Natours',
+        'description': 'lorem ipsum',
+        'thumbnail': 'https://www.tonnessen.com/assets/natours.png',
+        'link': 'https://www.tonnessen.com/projects/natours/',
+        'github': 'https://github.com/Emizzari/natours',
+        'school': null,
+        'languages': ['HTML', 'Sass']
+    },
+    {
+        'id': 4,
+        'slug': 'trillo',
+        'name': 'Trillo',
+        'description': 'lorem ipsum',
+        'thumbnail': 'https://www.tonnessen.com/assets/trillo.png',
+        'link': 'https://www.tonnessen.com/projects/trillo/',
+        'github': 'https://github.com/Emizzari/trillo',
+        'school': null,
+        'languages': ['HTML', 'Sass', 'JavaScript']
+    },
+    {
+        'id': 5,
+        'slug': 'nexter',
+        'name': 'Nexter',
+        'description': 'lorem ipsum',
+        'thumbnail': 'https://www.tonnessen.com/assets/nexter.png',
+        'link': 'https://www.tonnessen.com/projects/nexter/',
+        'github': 'https://github.com/Emizzari/nexter',
+        'school': null,
+        'languages': ['HTML', 'Sass']
+    },
+    {
+        'id': 6,
+        'slug': 'spacex',
+        'name': 'Space X Microsite',
+        'description': 'lorem ipsum',
+        'thumbnail': 'https://www.tonnessen.com/assets/spacex.jpg',
+        'link': 'https://www.tonnessen.com/noroff/feu/project-exam-1/',
+        'github': 'https://github.com/Emizzari/24-05-2020_project-exam-1_emilie-tonnessen',
+        'school': {
+            'grade': 'B',
+            'brief': 'This was a School assignment.'
+        },
+        'languages': ['HTML', 'Sass', 'JavaScript']
+    },
+    {
+        'id': 7,
+        'slug': 'recycle',
+        'name': 'Re-Cycle',
+        'description': 'lorem ipsum',
+        'thumbnail': 'https://www.tonnessen.com/assets/recycle.jpg',
+        'link': 'https://www.tonnessen.com/noroff/feu/html-css/',
+        'github': 'https://github.com/Emizzari/emilie-tonnessen_html-css',
+        'school': null,
+        'languages': ['HTML', 'CSS']
+    },
+]
+
+export default projects;
