9b8949ee1682fc589958e4ed978ebc528d8bd151
Emizzari <emilie@tonnessen.com>
2021-03-31 18:09:46 +0200

    Creating a working search function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,16 @@
-const Search = () => {
+const Search = ({search}) => {
     return (
         <div className="search">
-            <input type="text" placeholder="Search.." className="search__input" />
+            <input 
+                type="text" 
+                placeholder="Search.." 
+                className="search__input"
+                onChange={search} />
             <button className="search__button">
                 <i className="search__icon far fa-search"></i>
             </button>
         </div>
-    )
+    );
 }
 
-export default Search
+export default Search;
--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -1,9 +1,37 @@
-import Projects from '../components/project/Projects'
-import Search from '../components/Search'
-
+import Project from '../components/project/Project';
+import Search from '../components/Search';
+import projectsJSON from './../server/projects';
+import { useEffect, useState } from 'react';
+import cross from '../assets/times-light.svg';
+//import magnifyingGlass from '../assets/search-regular.svg';
 
 const Portfolio = () => {
+    const [projects, setProjects] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+    const [/* searchFocus */, setSearchFocus] = useState(false);
+    const [searchIcon, setSearchIcon] = useState('');
+    const projectsArray = projectsJSON;
+    let search = searchValue.trim().toLowerCase();
+    
+    useEffect(() => {
+        setProjects(projectsArray);
+    
+        if(search.length > 0) {
+            setProjects(projectsArray.filter(project => {
+                return project.name.toLowerCase().match(search);
+            }));
+        } if (search.length === 0) {
+            setProjects(projectsArray);
+        }
+    }, [search, projectsArray]);
 
+    const searchHandler = (e) => {
+        setSearchValue(e.target.value);
+        setSearchFocus(true);
+        setSearchIcon(cross);
+    }
+
+    //console.log(projects);
 
     return (
         <div className="portfolio" id="portfolio">
@@ -12,10 +40,22 @@ const Portfolio = () => {
                 <span></span>
             </h2>
             
-            <Search />
+            <Search
+                search={searchHandler}
+                icon={searchIcon} />
 
-            <Projects />
+            <div className="projects">
+                {projects.map(project => {
+                    return (
+                        <Project 
+                            key={project.id}
+                            image={project.thumbnail}
+                            title={project.name}
+                            github={project.github} />
+                    );
+                })}
 
+            </div>
         </div>
     );
 }
