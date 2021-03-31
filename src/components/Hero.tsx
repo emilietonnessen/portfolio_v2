d9590bd15544e12b908e0cba459711acb3bf1eb6
Emizzari <emilie@tonnessen.com>
2021-03-31 23:17:25 +0200

    Adding props for more dynamic data

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import Button from './UI/Button';
 
 interface HeroProps {
     headline: string;
@@ -6,3 +7,14 @@ interface HeroProps {
 }
 
 const Hero: React.FC<HeroProps> = ({headline, jobTitle, children}) => {
+    return (
+        <div className="hero">
+            <h1 className="hero__headline">{headline}</h1>
+            <h2 className="hero__job-title">{jobTitle}</h2>
+            <p className="hero__text">{children}</p>
+            <Button link="#portfolio">View my portfolio</Button>
+        </div>
+    );
+}
+
+export default Hero;
