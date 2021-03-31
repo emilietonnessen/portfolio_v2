a53d548f29d88c76c98d6881b43e7ccb1d0ae6b7
Emizzari <emilie@tonnessen.com>
2021-03-31 16:36:15 +0200

    update

--- src/components/Layout/nav/Sidebar.js
+++ src/components/Layout/nav/Sidebar.js
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import Brand from './Brand';
 import Media from './Media';
 import Menu from './Menu';
 
 const Sidebar = () => {
+    const [isMenuOpen, setMenuOpen] = useState('false');
+
+    const openMenuHandler = () => {
+        setMenuOpen(!isMenuOpen); 
+    }
+
     return (
         <div className="sidebar">
             <Brand />
-            <Menu />
+            <Menu  isMenuOpen={isMenuOpen ? true : false } />
             <Media />
+            
+            <div className="nav-icon" onClick={() => openMenuHandler()}>
+                <button className="nav-icon__button"></button>
+            </div>
+            
         </div>
     );
 }
