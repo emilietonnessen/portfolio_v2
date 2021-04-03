2f123fcb1a10fe5e3295f5c236f1bad0c4d7cbcc
Emizzari <emilie@tonnessen.com>
2021-04-03 16:27:37 +0200

    Fixing height issues in design on the nav bar and brand

--- src/components/Layout/nav/Menu.js
+++ src/components/Layout/nav/Menu.js
@@ -1,21 +1,52 @@
+import { useState } from "react";
+
 const Menu = ({isMenuOpen}) => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+
+    const open = 'menu menu-open';
+    const closed = 'menu'
+
+    console.log(isMenuOpen);
+    
+    let classes = closed;
+    
+    const onClickHandler = () => {
+        console.log('clicked');
+        
+        
+        if (!menuOpen) {
+            classes = open;
+            setMenuOpen(true);
+        }
+        
+        if (menuOpen === true) {
+            setMenuOpen(false);
+            classes = closed;
+        }
+    }
 
     return (
-        <nav className={isMenuOpen ? 'menu menu-open' : 'menu'}>
+        <nav className={
+            menuOpen ? classes : isMenuOpen ? closed : open 
+            
+            
+        
+        }>
             <ul className="menu__list">
-                <li className="menu__item">
-                    <a href="#portfolio" className="menu__link">
+                <li className="menu__item" >
+                    <a href="#portfolio" className="menu__link" onClick={() => onClickHandler()}>
                         <i className="fas fa-palette"></i> 
                         Portfolio
                     </a>
                 </li>
-                <li className="menu__item">
+                <li className="menu__item" onClick={() => onClickHandler()}>
                     <a href="#about" className="menu__link">
                         <i className="fas fa-user"></i> 
                         about
                     </a>
                 </li>
-                <li className="menu__item">
+                <li className="menu__item" onClick={() => onClickHandler()}>
                     <a href="#contact" className="menu__link">
                         <i className="fas fa-envelope"></i> 
                         contact
--- src/components/Layout/nav/Sidebar.js
+++ src/components/Layout/nav/Sidebar.js
@@ -13,7 +13,31 @@ const Sidebar = () => {
     return (
         <div className="sidebar">
             <Brand />
-            <Menu  isMenuOpen={isMenuOpen ? true : false } />
+            {/* <Menu  isMenuOpen={isMenuOpen ? true : false } /> */}
+            <nav className={isMenuOpen ? 'menu' : 'menu menu-open' }>
+                <ul className="menu__list">
+                    <li className="menu__item" >
+                        <a href="#portfolio" className="menu__link" onClick={() => openMenuHandler()}>
+                            <i className="fas fa-palette"></i> 
+                            Portfolio
+                        </a>
+                    </li>
+                    <li className="menu__item" onClick={() => openMenuHandler()}>
+                        <a href="#about" className="menu__link">
+                            <i className="fas fa-user"></i> 
+                            about
+                        </a>
+                    </li>
+                    <li className="menu__item" onClick={() => openMenuHandler()}>
+                        <a href="#contact" className="menu__link">
+                            <i className="fas fa-envelope"></i> 
+                            contact
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+
             <Media />
             
             <div className="nav-icon" onClick={() => openMenuHandler()}>
