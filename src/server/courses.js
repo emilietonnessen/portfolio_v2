4a9907d47c73e648d28a5fadfca1eb8da36c8c95
Emilie Tønnessen <emilie@tonnessen.com>
2021-10-13 20:55:26 +0200

    Update certificates

--- src/server/courses.js
+++ src/server/courses.js
@@ -1,52 +1,22 @@
 const courses = [
-    {
-        'id': 1,
-        'slug': 'advanced-css',
-        'name': 'Advanced CSS and Sass',
-        'link': 'https://www.udemy.com/course/advanced-css-and-sass/',
-        'certificate': 'https://www.tonnessen.com/assets/certificate_advanced-css-and-sass.jpg',
-        'progress': null
-    },
-    {
-        'id': 2,
-        'slug': 'react-the-complete-guide',
-        'name': 'React - The Complete Guide',
-        'link': 'https://www.udemy.com/course/react-the-complete-guide-incl-redux/',
-        'certificate': null,
-        'progress': '55%'
-    },
-    {
-        'id': 3,
-        'slug': 'react-and-typescript',
-        'name': 'React and TypeScript',
-        'link': 'https://www.udemy.com/course/react-and-typescript-build-a-portfolio-project/',
-        'certificate': null,
-        'progress': '20%'
-    },
-    {
-        'id': 4,
-        'slug': 'advanced-javascript',
-        'name': 'The Complete JavaScript course 2021',
-        'link': 'https://www.udemy.com/course/the-complete-javascript-course/',
-        'certificate': null,
-        'progress': '0%'
-    },
-    {
-        'id': 5,
-        'slug': 'node-express-mongodb',
-        'name': 'Node.js, Express, MongoDB & More',
-        'link': 'https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/',
-        'certificate': null,
-        'progress': '0%'
-    },
-    {
-        'id': 6,
-        'slug': 'wordpress',
-        'name': 'Become a WordPress Developer',
-        'link': 'https://www.udemy.com/course/become-a-wordpress-developer-php-javascript/',
-        'certificate': null,
-        'progress': '20%'
-    }
+  {
+    id: 1,
+    slug: 'advanced-css',
+    name: 'Advanced CSS and Sass',
+    link: 'https://www.udemy.com/course/advanced-css-and-sass/',
+    certificate:
+      'https://www.tonnessen.com/assets/certificate_advanced-css-and-sass.jpg',
+    progress: null,
+  },
+  {
+    id: 2,
+    slug: 'unit-testing-for-typescript-&-nodeJs-developers-with-jest',
+    name: 'Unit Testing with Jest',
+    link: 'https://www.udemy.com/course/unit-testing-typescript-nodejs/',
+    certificate:
+      'https://www.tonnessen.com/assets/unit-testing-for-typescript-&-nodejs-developers-with-jest.jpg',
+    progress: null,
+  },
 ]
 
-export default courses;
+export default courses
